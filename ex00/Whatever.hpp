78e69e0481312c069efdafd54dbf8eeff612c27f
bumkand <jakub.andrasi@gmail.com>
2026-04-15 17:21:01 +0200

    ex00 templates basics done

--- ex00/Whatever.hpp
+++ ex00/Whatever.hpp
@@ -3,7 +3,30 @@
 
 #include <iostream>
 
+template <typename T> void swap(T &x, T &y)
+{
+	T z;
 
-static void swap();
+	z = x;
+	x = y;
+	y = z;
+
+}
+
+template <typename T> T min(T x, T y)
+{
+	if (x < y)
+		return x;
+	else
+		return y;
+}
+
+template <typename T> T max(T x, T y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+}
 
 #endif
