0d297f6b1291ee7e04a4bf5f1cd2f4507f7412e1
bumkand <jakub.andrasi@gmail.com>
2026-04-17 16:51:24 +0200

    ex02

---
+++ ex02/Array.hpp
@@ -0,0 +1,14 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template <typename T> class Array
+{
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array& other);
+		Array& operator=(const Array& other);
+		
+}
+
+#endif
